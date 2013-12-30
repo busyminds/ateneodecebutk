128bc7c35d680c9e666dd7503094b53184d7c1c3
Noel Martin Llevares <dashmug@gmail.com>
2013-12-30 09:00:12 +0800

    new changes

--- ateneodecebutk/gradebook/urls.py
+++ ateneodecebutk/gradebook/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from gradebook import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^downloads/$', views.downloads, name='downloads'),
 )
--- ateneodecebutk/gradebook/views.py
+++ ateneodecebutk/gradebook/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("You are at /gradebook.")
+
+def downloads(request):
+    return HttpResponse("You are at /downloads.")
