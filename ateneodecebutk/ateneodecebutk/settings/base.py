53b7b0d21aa75b11824d2b7cd563b87353eea97f
Noel Martin Llevares <noelmartin@gmail.com>
2014-01-05 21:03:55 +0800

    some changes

--- ateneodecebutk/ateneodecebutk/settings/base.py
+++ ateneodecebutk/ateneodecebutk/settings/base.py
@@ -43,12 +43,12 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'django.contrib.admin',
+    'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south',
     'ateneodecebutk_app',
     'gradebook',
     'competencies',
---
+++ django_secret_key_generator.py
@@ -0,0 +1,20 @@
+"""
+Two things are wrong with Django's default `SECRET_KEY` system:
+
+1. It is not random but pseudo-random
+2. It saves and displays the SECRET_KEY in `settings.py`
+
+This snippet
+1. uses `SystemRandom()` instead to generate a random key
+2. saves a local `secret.txt`
+
+The result is a random and safely hidden `SECRET_KEY`.
+"""
+# import string
+
+try:
+    SECRET_KEY
+except NameError:
+    import random
+    SECRET_KEY = ''.join([random.SystemRandom().choice('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\()*+,-./:;<=>?@[\\]^_`{|}~') for i in range(50)])
+    print SECRET_KEY
