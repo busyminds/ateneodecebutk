a53eb52a87320e2e0b09669ba5247ab1e6e4ac7b
Noel Martin Llevares <dashmug@gmail.com>
2014-01-14 06:21:59 +0800

    improved templates

--- ateneodecebutk/gradebook/gradebook.py
+++ ateneodecebutk/gradebook/gradebook.py
@@ -1,4 +1,4 @@
-SUBJECTS = (
+SUBJECT_CODES = [
     ('CHI', 'Chinese'),
     ('CLF', 'CLF'),
     ('FIL', 'Filipino'),
@@ -12,10 +12,10 @@ SUBJECTS = (
     ('PE', 'PE'),
     ('HE', 'HE'),
     ('COM', 'Computer')
-)
+]
 
-SECTIONS = (
-    (
+SECTION_CODES = [
+    [
         ('G1A', '1-Sales'),
         ('G1B', '1-Azevedo'),
         ('G1C', '1-Imbert'),
@@ -23,51 +23,50 @@ SECTIONS = (
         ('G1E', '1-Mayer'),
         ('G1F', '1-Hoyos'),
         ('G1G', '1-Pro')
-    ),
-    (
+    ],
+    [
         ('G2A', '2-De Brito'),
         ('G2B', '2-Acquaviva'),
         ('G2C', '2-Pacheco'),
         ('G2D', '2-Spinola'),
         ('G2E', '2-Jerome'),
         ('G2F', '2-Ricci')
-    ),
-    (
+    ],
+    [
         ('G3A', '3-Miki'),
         ('G3B', '3-Borgia'),
         ('G3C', '3-Rodriguez'),
         ('G3D', '3-Arrowsmith'),
         ('G3E', '3-Colombiere')
-    ),
-    (
+    ],
+    [
         ('G4A', '4-Berchmans'),
         ('G4B', '4-Jogues'),
         ('G4C', '4-Campion'),
         ('G4D', '4-Pignatelli'),
         ('G4E', '4-Del Castillo')
-    ),
-    (
+    ],
+    [
         ('G5A', '5-Xavier'),
         ('G5B', '5-Faber'),
         ('G5C', '5-Hurtado'),
         ('G5D', '5-Garate'),
         ('G5E', '5-San Vitores')
-    ),
-    (
+    ],
+    [
         ('G6A', '6-Loyola'),
         ('G6B', '6-Regis'),
         ('G6C', '6-Claver'),
         ('G6D', '6-Bellarmine'),
         ('G6E', '6-Canisius'),
         ('G6F', '6-Gonzaga')
-    )
-)
+    ]
+]
 
 def get_subjects(grade_level):
-    subjects = list(SUBJECTS)
+    subjects = list(SUBJECT_CODES)
     if grade_level < 4:
         subjects.remove(('HE', 'HE'))
         if grade_level < 3:
             subjects.remove(('SCI', 'Science'))
-
     return subjects
--- ateneodecebutk/gradebook/views.py
+++ ateneodecebutk/gradebook/views.py
@@ -7,28 +7,57 @@ import json
 
 from ateneodecebutk.settings.base import BASE_DIR
 
-# Create your views here.
 def index(request):
-    grade_levels = range(1,7)
+    class_table = [
+        {
+            'grade_level': 1,
+            'sections': gradebook.SECTION_CODES[0],
+            'subjects': gradebook.get_subjects(1)
+        },
+        {
+            'grade_level': 2,
+            'sections': gradebook.SECTION_CODES[1],
+            'subjects': gradebook.get_subjects(2)
+        },
+        {
+            'grade_level': 3,
+            'sections': gradebook.SECTION_CODES[2],
+            'subjects': gradebook.get_subjects(3)
+        },
+        {
+            'grade_level': 4,
+            'sections': gradebook.SECTION_CODES[3],
+            'subjects': gradebook.get_subjects(4)
+        },
+        {
+            'grade_level': 5,
+            'sections': gradebook.SECTION_CODES[4],
+            'subjects': gradebook.get_subjects(5)
+        },
+        {
+            'grade_level': 6,
+            'sections': gradebook.SECTION_CODES[5],
+            'subjects': gradebook.get_subjects(6)
+        }
+    ]
 
-    subject_array = []
+    gradebook_data = {}
 
-    for grade_level in range(1,7):
-        subject_array.append(gradebook.get_subjects(grade_level))
-
-    gradebook_data = []
-
-    for level in grade_levels:
-        glob_dir = os.path.join(BASE_DIR, 'data/assessments/levels/' + str(level))
+    for level in class_table:
+        glob_dir = os.path.join(BASE_DIR, 'data/assessments/levels/'
+            + str(level['grade_level']))
         for f in glob.glob(glob_dir + '/*.json'):
             json_file = open(f)
             json_data = json.load(json_file)
-            gradebook_data.append(json_data)
+            codes = os.path.basename(f).rstrip('.json').split('-')
+            gradebook_data[codes[0]]={}
+            gradebook_data[codes[0]][codes[1]] = json_data
 
     context = {
-        'grade_levels': grade_levels,
-        'sections': gradebook.SECTIONS,
-        'subject_array': subject_array,
-        'gradebook_data': gradebook_data
+        # 'grade_levels': grade_levels,
+        # 'sections': gradebook.SECTIONS,
+        # 'subject_array': subject_array,
+        'gradebook_data': gradebook_data,
+        'class_table': class_table
     }
     return render(request, 'gradebook/index.html', context)
