253f9eda54853fecff8668a7a474b99c755fbd5a
Noel Martin Llevares <noelmartin@gmail.com>
2014-01-13 12:24:26 +0800

    i give up

--- ateneodecebutk/gradebook/views.py
+++ ateneodecebutk/gradebook/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 import gradebook
+import glob
+import os
+import json
+
+from ateneodecebutk.settings.base import BASE_DIR
 
 # Create your views here.
 def index(request):
@@ -11,9 +16,19 @@ def index(request):
     for grade_level in range(1,7):
         subject_array.append(gradebook.get_subjects(grade_level))
 
+    gradebook_data = []
+
+    for level in grade_levels:
+        glob_dir = os.path.join(BASE_DIR, 'data/assessments/levels/' + str(level))
+        for f in glob.glob(glob_dir + '/*.json'):
+            json_file = open(f)
+            json_data = json.load(json_file)
+            gradebook_data.append(json_data)
+
     context = {
         'grade_levels': grade_levels,
         'sections': gradebook.SECTIONS,
         'subject_array': subject_array,
+        'gradebook_data': gradebook_data
     }
     return render(request, 'gradebook/index.html', context)
