fc63430dc612ba7abceffae16b8c799e6e77fef1
Noel Martin Llevares <dashmug@gmail.com>
2014-02-05 04:43:33 +0800

    Add production settings.

--- ateneodecebutk/ateneodecebutk/settings/production.py
+++ ateneodecebutk/ateneodecebutk/settings/production.py
@@ -8,8 +8,12 @@ ALLOWED_HOSTS = ['ateneodecebu.tk']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
-
-
 ADMINS = ('noelmartin@gmail.com')
 
 MANAGERS = ('noelmartin@gmail.com')
+
+DEFAULT_FROM_EMAIL = 'noelmartin@gmail.com'
+
+SERVER_EMAIL = 'noelmartin@gmail.com'
+
+USE_X_FORWARDED_HOST = True
