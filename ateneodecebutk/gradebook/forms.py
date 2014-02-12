e3b336cf5d87ddd818e58ecedbbf01ac0f59ffe0
Noel Martin Llevares <noelmartin@gmail.com>
2014-02-12 16:19:34 +0800

    cleanup unused code

--- ateneodecebutk/gradebook/forms.py
+++ ateneodecebutk/gradebook/forms.py
@@ -3,3 +3,4 @@ from django import forms
 class UploadFileForm(forms.Form):
     # title = forms.CharField(max_length=50)
     file  = forms.FileField(label='Browse for your ECR file...')
+    # file.css_classes('form-control')
