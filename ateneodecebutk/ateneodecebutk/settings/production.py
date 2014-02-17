b53b0d59f4d176494a04a092fcb3563bb91762b6
Noel Martin Llevares <noelmartin@gmail.com>
2014-02-17 16:07:17 +0800

    Corrected password

--- ateneodecebutk/ateneodecebutk/settings/production.py
+++ ateneodecebutk/ateneodecebutk/settings/production.py
@@ -23,5 +23,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_HOST_USER ='noelmartin@gmail.com'
-EMAIL_HOST_PASSWORD = 'SourceSplitThirdProperly'
+EMAIL_HOST_PASSWORD = 'SourceSplitThirdProperly()_'
 EMAIL_USE_TLS = True
