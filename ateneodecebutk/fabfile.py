5965a035d8ec95a336801a4140821b686dbbc27e
Noel Martin Llevares <dashmug@gmail.com>
2014-02-21 01:27:55 +0800

    Add Fabric deployment script

---
+++ ateneodecebutk/fabfile.py
@@ -0,0 +1,16 @@
+from __future__ import with_statement
+from fabric.api import *
+# from fabric.contrib.console import confirm
+
+env.hosts = ['128.199.237.125']
+
+def prepare_deploy():
+    local("git add . -A && git commit")
+    local("git push")
+
+def deploy():
+    prepare_deploy()
+    code_dir = '/home/noel/ateneodecebutk'
+    with cd(code_dir):
+        run("git pull")
+        run("sudo service uwsgi restart")
