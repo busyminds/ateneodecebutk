3e8a830dbdce6e20f07cda9faf27ddcb1184d327
Noel Martin Llevares <noelmartin@gmail.com>
2014-02-26 15:54:20 +0800

    Register new models in admin

--- ateneodecebutk/registry/admin.py
+++ ateneodecebutk/registry/admin.py
@@ -3,6 +3,8 @@ from .models import Teacher
 from .models import Section
 from .models import Subject
 from .models import Course
+from .models import SchoolYear
+
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('section', 'subject', 'teacher', 'schedule')
@@ -11,4 +13,5 @@ class CourseAdmin(admin.ModelAdmin):
 admin.site.register(Teacher)
 admin.site.register(Section)
 admin.site.register(Subject)
+admin.site.register(SchoolYear)
 admin.site.register(Course, CourseAdmin)
